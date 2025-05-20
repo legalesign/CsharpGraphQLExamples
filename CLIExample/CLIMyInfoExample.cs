d22bdd278224a392e6bb40253930d86f4a091ffb
Alex Weinle <alex.weinle@gmail.com>
2025-05-20 10:20:19 +0100

    Comment removed.

--- CLIExample/CLIMyInfoExample.cs
+++ CLIExample/CLIMyInfoExample.cs
@@ -17,8 +17,6 @@ using System.Threading.Tasks;
 
 namespace CLILegalesignExamples
 {
-    // These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
-
     class CLIMyInfoExample
     {
         static async Task Main(string[] args)
